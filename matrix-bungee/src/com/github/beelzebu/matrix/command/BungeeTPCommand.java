44dad8e1cbf12468a202c8b88baaaa5ca8744aa4
Beelzebu <jsuarez.lara@outlook.com>
2020-08-11 19:53:17 -0400

    Fixed NPE on BungeeTPCommand and send error message when running command from console

--- matrix-bungee/src/com/github/beelzebu/matrix/command/BungeeTPCommand.java
+++ matrix-bungee/src/com/github/beelzebu/matrix/command/BungeeTPCommand.java
@@ -1,7 +1,12 @@
 package com.github.beelzebu.matrix.command;
 
+import com.github.beelzebu.matrix.api.i18n.I18n;
+import com.github.beelzebu.matrix.api.i18n.Message;
+import com.github.beelzebu.matrix.api.util.StringUtils;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -18,9 +23,16 @@ public class BungeeTPCommand extends Command {
     public void execute(CommandSender commandSender, String[] args) {
         if (commandSender instanceof ProxiedPlayer) {
             if (args.length == 1) {
-                ProxiedPlayer pp = (ProxiedPlayer) commandSender;
-                pp.connect(ProxyServer.getInstance().getServerInfo(args[0]));
+                ProxiedPlayer proxiedPlayer = (ProxiedPlayer) commandSender;
+                ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(args[0]);
+                if (serverInfo != null) {
+                    proxiedPlayer.connect(serverInfo);
+                } else {
+                    proxiedPlayer.sendMessage(TextComponent.fromLegacyText(StringUtils.replace("&cThere is no server with the specified name.")));
+                }
             }
+        } else {
+            commandSender.sendMessage(TextComponent.fromLegacyText(I18n.tl(Message.GENERAL_NO_CONSOLE, I18n.DEFAULT_LOCALE)));
         }
     }
 }
