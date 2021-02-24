2761a26e2b9e45b4bb6e9f3c7235fe33f603a966
Beelzebu <jsuarez.lara@outlook.com>
2021-02-24 15:14:16 -0300

    Updated RegisterHumildeCommand

--- matrix-bungee/src/main/java/com/github/beelzebu/matrix/bungee/command/RegisterHumildeCommand.java
+++ matrix-bungee/src/main/java/com/github/beelzebu/matrix/bungee/command/RegisterHumildeCommand.java
@@ -5,6 +5,7 @@ import com.github.beelzebu.matrix.player.MongoMatrixPlayer;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Date;
 import java.util.UUID;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
@@ -23,14 +24,16 @@ public class RegisterHumildeCommand extends Command {
     public void execute(CommandSender commandSender, String[] strings) {
         try {
             Files.readAllLines(new File(Matrix.getAPI().getPlugin().getDataFolder(), "miembros.txt").toPath()).forEach(line -> {
-                Matrix.getAPI().getPlayerManager().getPlayerByName(line).thenAccept(matrixPlayer -> {
+                ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), "lp user " + line + " parent add humilde humildadcraft");
+                Matrix.getAPI().getDatabase().getPlayerByName(line).thenAccept(matrixPlayer -> {
                     if (matrixPlayer == null) {
                         matrixPlayer = new MongoMatrixPlayer(UUID.nameUUIDFromBytes(("OfflinePlayer:" + line).getBytes()), line);
                     }
+                    ((MongoMatrixPlayer) matrixPlayer).setRegistration(new Date());
                     matrixPlayer.setPremium(true);
+                    matrixPlayer.setRegistered(true);
                     matrixPlayer.save().join();
                 });
-                ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), "lp user " + line + " parent add humilde humildadcraft");
             });
         } catch (IOException e) {
             e.printStackTrace();
